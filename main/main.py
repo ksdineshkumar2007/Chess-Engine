3ec34d0326e9454da464c76eb02458685074ca35
ksdineshkumar2007 <ksdineshkumar2007@gmail.com>
2026-06-22 20:12:40 +0530

    pawn moves commited

--- main/main.py
+++ main/main.py
@@ -1,7 +1,8 @@
 from board_file import create_board
-from print_b_file import print_board
+from pawn_move import pawn_move
 def main():
-    Board=create_board()
-    print_board(Board)
+    board=create_board()
+    print(pawn_move(board, 1, 0, "black"))   
+    print(pawn_move(board, 5, 0, "white"))  
 if __name__=="__main__":
     main()
---
+++ main/pawn_move.py
@@ -0,0 +1,31 @@
+def pawn_move(board,row,col,color):
+    valid_moves=[]
+    if color=="white":
+        if row==6:
+            if board[row-1][col]==0 and board[row-2][col]==0:
+                valid_moves.extend([(row-1,col),(row-2,col)])
+            if board[row-1][col]==0 and board[row-2][col]!=0:
+                valid_moves.append((row-1,col))
+        if 0<row<7 and board[row-1][col+1]<0 and col!=7:
+            valid_moves.append((row-1,col+1))
+        if 0<row<7 and board[row-1][col-1]<0 and col!=0:
+            valid_moves.append((row-1,col-1))
+        if 0<row<6 and board[row-1][col]==0:
+            valid_moves.append((row-1,col))
+        if row==0:
+            valid_moves.append((row-1,col)) 
+    if color=="black":
+        if row == 1:
+            if board[row+1][col] == 0 and board[row+2][col] == 0:
+                valid_moves.extend([(row+1,col),(row+2,col)])
+            if board[row+1][col] == 0 and board[row+2][col] != 0:
+                valid_moves.append((row+1,col))
+        if 0 < row < 7 and board[row+1][col+1] > 0 and col != 7:
+            valid_moves.append((row+1,col+1))
+        if 0 < row < 7 and board[row+1][col-1] > 0 and col != 0:
+            valid_moves.append((row+1,col-1))
+        if 1 < row < 7 and board[row+1][col] == 0:
+            valid_moves.append((row+1,col))
+        if row == 7:
+            valid_moves.append((row+1,col))
+    return valid_moves         
