c0f391f9fa729f72cd9c1f4199e815693d7973c6
ksdineshkumar2007 <ksdineshkumar2007@gmail.com>
2026-06-23 16:44:22 +0530

    all moves

---
+++ main/get_all_moves.py
@@ -0,0 +1,21 @@
+from bishop_moves import bishop_move
+from pawn_move import pawn_move
+from queen_moves import queen_move
+from rook_moves import rook_move
+from king_moves import king_move
+from knight_moves import knight_move
+
+def get_allmoves(board, color):
+    moves = []
+    piece_moves = {
+        1: pawn_move, 2: rook_move, 3: bishop_move,
+        4: knight_move, 5: queen_move, 6: king_move
+    }
+    for i in range(8):
+        for j in range(8):
+            piece = board[i][j]
+            if (color == "white" and piece > 0) or (color == "black" and piece < 0):
+                move_fn = piece_moves[abs(piece)]
+                for move in move_fn(board, i, j, color):
+                    moves.append(((i,j), move))
+    return moves
--- main/queen_moves.py
+++ main/queen_moves.py
@@ -1,2 +1,4 @@
+from rook_moves import rook_move
+from bishop_moves import bishop_move
 def queen_move(board, row, col, color):
     return rook_move(board, row, col, color) + bishop_move(board, row, col, color)
